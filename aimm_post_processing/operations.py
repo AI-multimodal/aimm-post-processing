9e00a7e38809bb7388f35e51900a96320cc4d060
Zhu <zhul9311@gmail.com>
2022-05-05 22:13:45 -0400

    Add RemoveBackground Operator

--- aimm_post_processing/operations.py
+++ aimm_post_processing/operations.py
@@ -7,6 +7,8 @@ from monty.json import MSONable
 import numpy as np
 import pandas as pd
 from scipy.interpolate import InterpolatedUnivariateSpline
+from sklearn.linear_model import LinearRegression
+
 
 
 class Operator(MSONable):
@@ -128,5 +130,76 @@ class StandardizeGrid(Operator):
                 **interpolated_univariate_spline_kwargs,
             )
             new_data[column] = ius(new_grid)
+
+        return {"data": pd.DataFrame(new_data), "metadata": metadata}
+
+
+class RemoveBackground(Operator):
+    """Fit the pre-edge region to a victoreen function and subtract it from the spectrum.
+    """
+    def __call__(
+        self,
+        dfClient,
+        *,
+        x0,
+        xf, 
+        x_column="energy",
+        y_columns=["mu"],
+        victoreen_order = 0
+    ):
+        """Subtract background from data.
+
+        Fit the pre-edge data to a line with slope, and subtract slope info from data.
+
+        Parameters
+        ----------
+        dfClient : tiled.client.dataframe.DataFrameClient
+        x0 : float
+            The lower bound of energy range on which the background is fitted.
+        xf : flaot
+            The upper bound of energy range on which the background is fitted.
+        x_column : str, optional
+            References a single column in the DataFrameClient (this is the
+            "x-axis"). Default is "energy".
+        y_columns : list, optional
+            References a list of columns in the DataFrameClient (these are the
+            "y-axes"). Default is ["mu"].
+        victoreen_order : int
+            The order of Victoreen function. The selected data is fitted to Victoreen pre-edge 
+            function (in which one fits a line to μ(E)*E^n for some value of n. Default is 0,
+            which is a linear fit.
+
+        Returns
+        -------
+        dict
+            A dictionary of the data and metadata. The data is a
+            :class:`pd.DataFrame`, and the metadata is itself a dictionary.
+        
+        Notes
+        -----
+        `LinearRegression().fit()` takes 2-D arrays as in put. This can be explored
+        for batch processing of multiple spectra
+
+        """
+        kwargs = {
+            key: value
+            for key, value in locals().items()
+            if key not in ["self", "dfClient"]
+        }
+
+        data, metadata = self._preprocess_DataFrameClient(dfClient)
+        metadata["post_processing"]["kwargs"] = kwargs
+
+        bg_data = data.loc[(data[x_column] >= x0) * (data[x_column] < xf)]
+
+        new_data = {x_column: data[x_column]}
+        for column in y_columns:
+            y = bg_data[column] * bg_data[x_column]**victoreen_order
+            reg = LinearRegression().fit(
+                bg_data[x_column].to_numpy().reshape(-1,1), 
+                y.to_numpy().reshape(-1,1)
+            )
+            background = reg.predict(data[x_column].to_numpy().reshape(-1,1))
+            new_data[column] = data.loc[:,column].to_numpy() - background.flatten()
 
         return {"data": pd.DataFrame(new_data), "metadata": metadata}
